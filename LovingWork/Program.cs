dca37a7f9ad39f1bcec80674b38937255034138c
VERTDIDER <ilungorbachev@gmail.com>
2020-11-03 15:48:47 +0300

    init commit

---
+++ LovingWork/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LovingWork
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum += i;
+            }
+        }
+    }
+}
