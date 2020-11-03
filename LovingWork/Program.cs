fa021406130b770bd933a6114c7a30a7cab0a9d8
VERTDIDER <ilungorbachev@gmail.com>
2020-11-04 01:12:54 +0300

    Вместо regex и matchcollection использовал foreach по чарам

--- LovingWork/Program.cs
+++ LovingWork/Program.cs
@@ -8,27 +8,41 @@ namespace LovingWork
     {
         public string GetStringInfo(string s)
         {
-            string letter = "a";
-            Regex regex = new Regex(letter);
-            MatchCollection match = regex.Matches(s);
-            int count_of_letter = match.Count;
-            string[] words = s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            Console.Write("letter - ");
+            char letter = Convert.ToChar(Console.Read());
+            Console.WriteLine();
+            //
+            // в реальности нужно использовать try catch OverFlowException при console read и конверте
+            //
+            int count_of_letter = 0;
+            foreach (char c in s)
+            {
+                count_of_letter = (c == letter) ? count_of_letter + 1 : count_of_letter;
+            }
+
+            string[] words = s.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
             int count_of_words = words.Length;
+
             int count_of_words_including_letter = 0;
             foreach (string word in words)
             {
-                if (word.Contains(letter)) {
+                if (word.Contains(letter.ToString())) {
                     count_of_words_including_letter++;
                 }
             }
-            return $"Количество слов - {count_of_words}, количество слов с данной буквой - {count_of_words_including_letter}, сколько раз буквы была найдена - {count_of_letter}";
+            //
+            //StringBuilder использовать не нужно, т.к. операция со строкой проходит один раз
+            //
+            return $"count of words - {count_of_words}, count of words including letter - {count_of_words_including_letter}, count of letter - {count_of_letter}";
         }
+
+
     }
     class MainClass
     {
         public static void Main(string[] args)
         {
-            string str = "rama mau djhed aejkncje weuf";
+            string str = "rama mau djhed aejkncje  weuf";
             LovingWorkWithStrings love = new LovingWorkWithStrings();
             Console.WriteLine(love.GetStringInfo(str));
         }
