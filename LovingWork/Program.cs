083f2d6c740294fe2bd59dc7ed1d121d00be32dd
VERTDIDER <ilungorbachev@gmail.com>
2020-11-04 00:05:46 +0300

    removed withString class

--- LovingWork/Program.cs
+++ LovingWork/Program.cs
@@ -3,40 +3,39 @@ using System.Globalization;
 using System.Text.RegularExpressions;
 
 namespace LovingWork
-{ 
-    class LovingWorkWithStrings
+{
+    class LovingWorkWithDate
     {
-        public string GetStringInfo(string s)
+        public string toDateWithTime(DateTime d) => d.ToString("g");
+        public string toDate(DateTime d) => d.ToString("d");
+        public DateTime toLogicDate(string s)
         {
-            string letter = "a";
-            Regex regex = new Regex(letter);
-            MatchCollection match = regex.Matches(s);
-            int count_of_letter = match.Count;
-            string[] words = s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-            int count_of_words = words.Length;
-            int count_of_words_including_letter = 0;
-            foreach (string word in words)
+            var provider = new CultureInfo("ru-RU");
+            try
             {
-                if (word.Contains(letter)) {
-                    count_of_words_including_letter++;
-                }
+                var dt = DateTime.ParseExact(s, "g", provider);
+                return dt;
             }
-            return $"Count of words - {count_of_words}, count of words including letter- {count_of_words_including_letter}, count of letter - {count_of_letter}";
+            catch (FormatException)
+            {
+                Console.WriteLine("Unacceptable data format");
+                return DateTime.Now;
+            }
         }
     }
     class MainClass
     {
         public static void Main(string[] args)
         {
-            //DateTime withtime = DateTime.Now;
-            //string date_str = "15.06.2015";
-            //LovingWorkWithDate love = new LovingWorkWithDate();
-            //Console.WriteLine("g: " + love.toDateWithTime(withtime));
-            //Console.WriteLine("d: " + love.toDate(withtime));
-            //Console.WriteLine(love.toLogicDate(date_str));
-            string str = "rama mau djhed aejkncje weuf";
-            LovingWorkWithStrings love = new LovingWorkWithStrings();
-            Console.WriteLine(love.GetStringInfo(str));
+            DateTime withtime = DateTime.Now;
+            string date_str = "15.06.2015";
+            LovingWorkWithDate love = new LovingWorkWithDate();
+            Console.WriteLine("g: " + love.toDateWithTime(withtime));
+            Console.WriteLine("d: " + love.toDate(withtime));
+            Console.WriteLine(love.toLogicDate(date_str));
+            //string str = "rama mau djhed aejkncje weuf";
+            //LovingWorkWithStrings love = new LovingWorkWithStrings();
+            //Console.WriteLine(love.GetStringInfo(str));
         }
     }
 }
