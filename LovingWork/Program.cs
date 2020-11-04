adc9495bd056c7332bf53f2eec9a7dfc407b06e4
VERTDIDER <ilungorbachev@gmail.com>
2020-11-04 03:24:54 +0300

    добавлен класс с датами в master

--- LovingWork/Program.cs
+++ LovingWork/Program.cs
@@ -6,6 +6,26 @@ using System.Text;
 
 namespace LovingWork
 {
+    class LovingWorkWithDate
+    {
+        public string toDateWithTime(DateTime d) => d.ToString("g");
+        public string toDate(DateTime d) => d.ToString("d");
+        public DateTime toLogicDate(string s)
+        {
+            var provider = new CultureInfo("ru-RU");
+            try
+            {
+                var dt = DateTime.ParseExact(s, "g", provider);
+                return dt;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Unacceptable data format");
+                return DateTime.Now;
+            }
+        }
+    }
+
     class LovingWorkWithStrings
     {
         public string GetStringInfo(string s)
@@ -59,6 +79,14 @@ namespace LovingWork
     {
         public static void Main(string[] args)
         {
+            //DateTime withtime = DateTime.Now;
+            //string date_str = "15.06.2015";
+            //LovingWorkWithDate love = new LovingWorkWithDate();
+            //Console.WriteLine("g: " + love.toDateWithTime(withtime));
+            //Console.WriteLine("d: " + love.toDate(withtime));
+            //Console.WriteLine(love.toLogicDate(date_str));
+
+
             string str = "rama mau djhed aejkncje  weuf";
             LovingWorkWithStrings love = new LovingWorkWithStrings();
             //Console.WriteLine(love.GetStringInfo(str));
